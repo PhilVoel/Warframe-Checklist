169b1c3ff2980a111c4ab668e54dc43093a86454
PhilVoel <philipp123.voeller@gmail.com>
2026-01-12 12:13:54 +0100

    Add service definition to flake.nix and autoformat it

--- flake.nix
+++ flake.nix
@@ -1,30 +1,87 @@
 {
-	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-25.05";
-		flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-	};
+  description = "Warframe Checklist application and service";
 
-	outputs = {flake-utils, nixpkgs, self, ...}:
-		flake-utils.lib.eachDefaultSystem (system:
-			let pkgs = nixpkgs.legacyPackages."${system}";
-			in {
-				devShells.default = pkgs.mkShell {
-					name = "WF Checklist";
-					packages = with pkgs; [
-						bacon
-						cargo
-						clippy
-						pkg-config
-						lua5_4
-					];
-					LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.lua5_4 ];
-				};
-				packages.default = pkgs.rustPlatform.buildRustPackage {
-					pname = "WF Checklist";
-					version = "0.1.0";
-					src = self;
-					cargoLock.lockFile = ./Cargo.lock;
-				};
-			}
-		);
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      self,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "WF Checklist";
+          packages = with pkgs; [
+            bacon
+            cargo
+            clippy
+            pkg-config
+            lua5_4
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.lua5_4 ];
+        };
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "WF Checklist";
+          version = "0.1.0";
+          src = self;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      }
+    )
+    // {
+      nixosModules.warframe-checklist =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        let
+          cfg = config.services.warframe-checklist;
+          stateDir = "/var/lib/wf-cheklist";
+        in
+        {
+          options.services.warframe-checklist = {
+            enable = lib.mkEnableOption "Warframe Checklist";
+            user = lib.mkOption {
+              type = lib.types.str;
+              default = "wf-cheklist";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            users.users.${cfg.user} = {
+              isSystemUser = true;
+              group = cfg.user;
+            };
+            users.groups.${cfg.user} = { };
+
+			systemd.tmpfiles.rules = [ "d ${stateDir} 0750 ${cfg.user} ${cfg.user} -" ];
+            systemd.services.warframe-checklist = {
+              description = "Warframe Checklist";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+
+
+              serviceConfig = {
+                ExecStart = "${self.packages.${pkgs.system}}/bin/wf_checklist";
+                Restart = "always";
+                User = cfg.user;
+                Group = cfg.group;
+                WorkingDirectory = stateDir;
+              };
+            };
+          };
+        };
+    };
 }
